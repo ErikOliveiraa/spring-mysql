eb63821cb358d93f93aa9081a0a64f5c7e02c2e3
ErikOliveiraa <erikg.oliveira25@gmail.com>
2026-08-20 23:22:09 -0300

    add project

---
+++ spring-mysql/src/test/java/br/com/erik/rest_springboot/Startup.java
@@ -0,0 +1,13 @@
+package br.com.erik.rest_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Startup {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
